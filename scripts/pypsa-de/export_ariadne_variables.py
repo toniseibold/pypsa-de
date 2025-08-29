57b53ec5f88f4ebd19c7e1891938a296a04e4867
Micha <michaellindner@pik-potsdam.de>
2025-08-29 16:35:51 +0200

    unpin ixmp4 (#118)
    * unpin ixmp4
    
    * fix minor pypsa error

--- scripts/pypsa-de/export_ariadne_variables.py
+++ scripts/pypsa-de/export_ariadne_variables.py
@@ -4296,7 +4296,7 @@ def get_economy(n, region):
     var = pd.Series()
 
     def get_tsc(n, country):
-        pypsa.options.params.statistics.drop_zero = False
+        pypsa.options.set_option("params.statistics.drop_zero", False)
         capex = n.statistics.capex(
             groupby=pypsa.statistics.groupers["name", "carrier"], nice_names=False
         )
